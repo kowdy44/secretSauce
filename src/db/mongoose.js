dff7991f64ca0fbfbd18459994b983c9a4625d55
Kowdy <kowdy44@gmail.com>
2019-05-05 20:19:15 +0530

    heroku mongodb connection setup

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI)
 let mongodbURI=process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 mongoose.connect(mongodbURI+'/appdb', {
     useNewUrlParser: true,
