707f4cda3ce9f8b7e427fa4e5d97831df905d6b0
Kowdy <kowdy44@gmail.com>
2021-07-01 22:05:46 +0530

    mongoDB cluster credentials added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI)
 let mongodbURI=process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
-mongodbURI = "mongodb+srv://dbUser:dbUserPassword@cluster0.hj8v8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongodbURI = "mongodb+srv://dbUser:dbUserPassword@cluster0.hj8v8.mongodb.net"
 mongoose.connect(mongodbURI+'/appdb', {
     useNewUrlParser: true,
     useCreateIndex: true,
