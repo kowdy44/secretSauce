51f868f8c0a09f055861574a2bee90b8ec841489
Kowdy <kowdy44@gmail.com>
2021-07-13 21:47:22 +0530

    minor changes email sending feature

--- src/utils/email/comment-email.js
+++ src/utils/email/comment-email.js
@@ -2,12 +2,12 @@
 const sendEmail = require('./send-email');
 const logging =require('../../log/logging')('comment-email.log');
 
-const sendEmailCommentAdded=function(emailId){
+const sendEmailCommentAdded=function(emailId,titleParagraph){
     let email=  {
           from: '"Team TravelBuddy 👻" <Supporttravelbuddy@gmail.com>', // sender address
           to: emailId, // list of receivers seperated by coma
           subject: "A Comment has been added to your paragraph!", // Subject line
-          text: "A Comment has been added. Login to see more details"
+          text: "A Comment has been added to paragraph titled '"+ titleParagraph+"'. Login to see more details"
         }
         sendEmail.sendMailNow(email).then((info)=>{
           logging.info(`Email sent: ${emailId}, messageId:${info.messageId}, info.response:${info.response} `);
