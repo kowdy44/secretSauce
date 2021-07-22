ede8fe8feac01014402c4ff93710da75ed208490
Kowdy <kowdy44@gmail.com>
2021-07-22 19:00:27 +0530

    changing mongodb URI

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,10 @@
 "use strict"
 const mongoose = require('mongoose');
-console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI)
+// console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI)
 // let mongodbURI=process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 let mongodbURI = "mongodb+srv://dbUser:dbUserPassword@cluster0.hj8v8.mongodb.net"
+process.env.MONGODB_URI = mongodbURI;
+console.log("process.env.MONGODB_URI : ",process.env.MONGODB_URI)
 mongoose.connect(mongodbURI+'/appdb', {
     useNewUrlParser: true,
     useCreateIndex: true,
