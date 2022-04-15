64c65dd34985d56f04f53c8a5fffb5f98a756c8f
Kowdy <kowdy44@gmail.com>
2022-04-15 11:49:41 +0530

    adding flag var for local DB connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,12 @@
 "use strict"
 const mongoose = require('mongoose');
-let mongodbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
+let mongodbURI;
+if (process.env.LOCAL_DB =='false') {
+    mongodbURI = process.env.MONGODB_URI;
+} else { 
+    mongodbURI = 'mongodb://127.0.0.1:27017'
+}
+console.log("process.env.LOCAL_DB : ", process.env.LOCAL_DB)
 console.log("mongodbURI : ", mongodbURI)
 // let mongodbURI = "mongodb+srv://dbUser:dbUserPassword@cluster0.hj8v8.mongodb.net"
 mongoose.connect(mongodbURI + '/appdb', {
